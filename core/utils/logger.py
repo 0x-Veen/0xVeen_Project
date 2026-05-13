05806efeed497c4a7afac76e5bb82b3c68e406fe
Abdelrahman Mohamed <abdelrahmanmohamedax1@gmail.com>
2026-05-13 07:42:40 -0400

    first commit

--- core/utils/logger.py
+++ core/utils/logger.py
@@ -1,29 +1,8 @@
 # core/utils/logger.py
 
-#def print_info(message):
-#    print(f"\033[94m[*] {message}\033[0m")  # Blue
-
-#def print_success(message):
-#    print(f"\033[92m[+] {message}\033[0m")  # Green
-
-#def print_error(message):
-#    print(f"\033[91m[-] {message}\033[0m")  # Red
-
-#def print_status(message):
-#    print(f"\033[96m[~] {message}\033[0m")  # Cyan
-
-#def print_good(message):
-#    print(f"\033[92m[✔] {message}\033[0m")  # Green tick
-
-#def print_warn(message):
-#    print(f"\033[93m[!] {message}\033[0m")  # Yellow
-
-
-# core/utils/logger.py
-
 from datetime import datetime
 
-# ألوان
+
 RESET = "\033[0m"
 BLUE = "\033[94m"
 GREEN = "\033[92m"
@@ -39,7 +18,7 @@ def print_info(msg):
 def print_success(msg):
     print(f"{GREEN}[{timestamp()}] [+] {msg}{RESET}")
 
-def print_good(msg):  # Alias for print_success
+def print_good(msg):  
     print_success(msg)
 
 def print_error(msg):
